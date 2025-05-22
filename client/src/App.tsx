8c4e213fe7151890193f63ac52044fc3125f75d3
Mark <markmaged9@gmail.com>
2025-05-22 16:09:45 +0200

    Login + Registration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
-import "./App.css";
 import { Routes, Route } from "react-router";
+import Login from "./pages/Authentication/Login/Login";
+import { Container, useMantineColorScheme } from "@mantine/core";
+import '@mantine/core/styles.css';
+import Signup from "./pages/Authentication/Signup/Signup";
+import UserDashboard from "./pages/User/UserDashboard/UserDashboard";
 
 const App = () => {
+  const {setColorScheme} = useMantineColorScheme()
+  setColorScheme("dark")
   return (
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<App />} />
-      <Route path="/signup" element={<App />} />
-      <Route path="/*" element={<h1>404 Page Not Found</h1>} />
-    </Routes>
+    <Container w={"100%"} h={"100%"}>
+      <Routes>
+        <Route path="/" element={<h1>Main Page</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/main" element={<UserDashboard/>} />
+        <Route path="/*" element={<h1>404 Page Not Found</h1>} />
+      </Routes>
+    </Container>
   );
 };
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <App/>
+      <MantineProvider
+        forceColorScheme="dark"
+        defaultColorScheme="dark"
+        theme={theme}
+      >
+        <App />
+      </MantineProvider>
     </BrowserRouter>
   </StrictMode>
 );
