a7fb1452cdb0267a79ab1629ce751c39d341d719
Mark <markmaged9@gmail.com>
2025-07-15 13:23:15 +0200

    Revert "init discussions page"
    This reverts commit ada3608263530e4a48af5d4cedb18ca8535435ad.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,20 @@
 import { Container, useMantineColorScheme } from "@mantine/core";
 import { Route, Routes } from "react-router";
 import Login from "./pages/Authentication/Login/Login";
+import "@mantine/core/styles.css";
 import Signup from "./pages/Authentication/Signup/Signup";
-import Discussions from "./pages/User/Discussions/Discussions";
 import UserDashboard from "./pages/User/UserDashboard/UserDashboard";
 
 const App = () => {
 	const { setColorScheme } = useMantineColorScheme();
 	setColorScheme("dark");
 	return (
-		<Container fluid w={"100%"} h={"100%"} p={0} m={0}>
+		<Container w={"100%"} h={"100%"}>
 			<Routes>
 				<Route path="/" element={<h1>Main Page: Deployment Working</h1>} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
-				<Route path="/main" element={<UserDashboard />}>
-					<Route path="discussions" element={<Discussions />} />
-					<Route path="feed" element={<div>feed</div>} />
-				</Route>
+				<Route path="/main" element={<UserDashboard />} />
 				<Route path="/*" element={<h1>404 Page Not Found</h1>} />
 			</Routes>
 		</Container>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,24 +3,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
-import "@mantine/core/styles.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const theme = createTheme({});
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<MantineProvider
-					forceColorScheme="dark"
-					defaultColorScheme="dark"
-					theme={theme}
-				>
-					<App />
-				</MantineProvider>
-			</BrowserRouter>
-		</QueryClientProvider>
+		<BrowserRouter>
+			<MantineProvider
+				forceColorScheme="dark"
+				defaultColorScheme="dark"
+				theme={theme}
+			>
+				<App />
+			</MantineProvider>
+		</BrowserRouter>
 	</StrictMode>,
 );
