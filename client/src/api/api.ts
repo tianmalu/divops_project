1a41bbc5c3e2304364044282b1eeda9583265da3
Mark <markmaged9@gmail.com>
2025-07-16 08:30:09 +0200

    linting

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export const QueryKeys = {
 	GET_DISCUSSIONS: "GET_DISCUSSIONS",
-}
+};
 
 export function useGetDiscussionsQuery() {
 	return useQuery({
