ba66070e1026ab64d2d1098ee5d140221caff2a1
Mark <markmaged9@gmail.com>
2025-07-16 09:29:08 +0200

    test terraform fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "ec2"
-  public_key = var.public_key
-}
-
 resource "aws_instance" "react_app" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  key_name                    = aws_key_pair.deployer.key_name
+  key_name                    = var.key_name
   associate_public_ip_address = true
 
   tags = {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,3 @@
-variable "public_key" {
-  type = string
-  description = "public SSH key"
-}
-
 variable "ami_id" {
   description = "AMI ID for EC2 (Ubuntu)"
   default     = "ami-020cba7c55df1f615"
@@ -11,4 +6,10 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 instance type"
   default     = "t2.nano"
+}
+
+variable "key_name" {
+  description = "Key pair name in AWS"
+  default = "ec2"
+  
 }
