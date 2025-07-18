82dfcd1795c35abdbcb8706b5f66836f3529e6c7
tianmalu <tinahzcc@gmail.com>
2025-07-18 14:05:00 +0200

    (genai) modified terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,21 @@ resource "aws_instance" "react_app" {
   }
 }
 
+resource "aws_instance" "genai_app" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "GenAI App"
+  }
+}
+
 output "ec2_public_ip" {
   value = aws_instance.react_app.public_ip
+}
+
+output "genai_public_ip" {
+  value = aws_instance.genai_app.public_ip
 }
