6674fe2e0d904817d644eb3a4bfd66b6e02db0d2
tianmalu <tinahzcc@gmail.com>
2025-07-19 19:47:47 +0200

    FIX: AWS

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,3 @@ resource "aws_instance" "genai_app" {
 output "ec2_public_ip" {
   value = aws_instance.react_app.public_ip
 }
-
-output "genai_public_ip" {
-  value = aws_instance.genai_app.public_ip
-}
